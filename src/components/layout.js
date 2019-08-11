3e6874a0fddd825cb186ca2162102d14053fe3cd
Ismail Opatola <opatolamails@gmail.com>
2019-08-11 06:28:10 +0100

    import scss  and sidebar component

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import SideBar from "./sidebar"
+import "../styles/index.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,20 +26,18 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <Header />
+      <SideBar />
+      <div>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}, Built by{" "}
+          <a
+            href="https://www.iodevelopment.com"
+            alt="Ismail Opatola Development"
+          >
+            IoDevelopment
+          </a>
         </footer>
       </div>
     </>
