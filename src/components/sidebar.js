a352b0e044c0c4459ba807a26de28f805aca0f1c
Ismail Opatola <opatolamails@gmail.com>
2019-08-18 19:32:59 +0100

    add navigation-links

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,27 +3,39 @@ import { Nav, NavItem, NavLink, Button } from "reactstrap"
 import { MdArrowDownward, MdMenu } from "react-icons/md"
 
 const Sidebar = () => {
+ 
   return (
-    <div className="sidebar">
-      <Button className="btn-dark">
-          <MdMenu className="sidebar-icon sidebar-icon-spacing"/>
+    <div className="sidebar" id="Sidebar">
+
+      <Button className="btn-dark sidebar--btn-menu sidebar-icon-spacing">
+        <MdMenu className="sidebar-icon" />
       </Button>
-      <div>
-        <Nav vertical hidden={true}>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
+
+      <div className="sidebar--nav">
+        <Nav vertical className="show-nav">
+          <NavItem className="show-nav-item">
+            <NavLink href="/#Showcase">ABOUT</NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
+          <hr />
+          <NavItem className="show-nav-item">
+            <NavLink href="/#Services">SERVICES</NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
+          <hr />
+          <NavItem className="show-nav-item">
+            <NavLink href="/#Projects">PROJECTS</NavLink>
+          </NavItem>
+          <hr />
+          <NavItem className="show-nav-item">
+            <NavLink href="/#Team">TEAM</NavLink>
           </NavItem>
         </Nav>
       </div>
 
-      <Button className="btn-dark" style={{justifySelf: 'end'}}>
-          <MdArrowDownward className="sidebar-icon" />
+      <Button
+        className="btn-dark sidebar--btn-down"
+        style={{ justifySelf: "end" }}
+      >
+        <MdArrowDownward className="sidebar-icon" />
       </Button>
     </div>
   )
