ea411f22f5e331c45162e7d0ac925a8750237ca5
Ismail Opatola <opatolamails@gmail.com>
2019-10-17 13:26:05 +0100

    create util -- project_details

--- src/util/projects.js
+++ src/util/projects.js
@@ -1,15 +1,98 @@
 const projects = [
   {
-    title: "",
-    description: "",
-    techStack: "",
+    title: "WUR",
+    description:
+      "Would-You-Rather App. A project Inspired by Udacity's React Developer Nano Degree programe. This app was part of the course capstone project..",
+    techStack: [
+      "Firebase Auth",
+      "React",
+      "Express",
+      "Firebase Hosting",
+      "Firebase Cloud Functions",
+      "Firebase Firestore",
+      "Material-UI",
+    ],
+    links: {
+      sourceCode: "https://github.com/Ismail-Opatola/wur-react-firebase-client",
+      websiteUrl: "https://would-you-rather-app-c5895.firebaseapp.com",
+      youtubeDemo: "",
+    },
+    imageUrl: "wur_img.png",
+  },
+  {
+    title: "Estatement",
+    description:
+      "A real estate management and architectural business website built with Gatsby and Netlify service.",
+    techStack: [
+      "Gatsby",
+      "React",
+      "Markdown",
+      "Reactstrap",
+      "Bootstrap",
+      "Netlify",
+    ],
+    links: {
+      sourceCode: "https://github.com/Ismail-Opatola/estatement_website",
+      websiteUrl: "https://inspiring-gates-7b3e39.netlify.com/",
+      youtubeDemo: "",
+    },
+    imageUrl: "estatement_img.png",
+  },
+  {
+    title: "Booho clone",
+    description:
+      "A clone of Boohoo Fashion UK. A client facing ecommerce web application built on Nodejs, Nextjs and Google Firebase Services.",
+    techStack: [
+      "Firebase Auth",
+      "Nextjs",
+      "Nodejs",
+      "Firebase Hosting",
+      "Firebase Cloud Functions",
+      "Firebase Firestore",
+      "Typescript",
+    ],
     links: {
       sourceCode: "",
       websiteUrl: "",
       youtubeDemo: "",
     },
-    imageUrl: ""
+    imageUrl: "",
+  },
+  {
+    title: "Boohoo CMS",
+    description:
+      "A fully featured Content Management System for Bohoo-Clone. An ecommerce platform built on Node, Nextjs and Google Firebase Services.",
+    techStack: [
+      "Firebase Auth",
+      "Nextjs",
+      "Nodejs",
+      "Firebase Hosting",
+      "Firebase Cloud Functions",
+      "Firebase Firestore",
+      "Typescript",
+    ],
+    links: {
+      sourceCode: "",
+      websiteUrl: "",
+      youtubeDemo: "",
+    },
+    imageUrl: "",
+  },
+  {
+    title: "Fit Tracker",
+    description: "A Fitness Tracking Mobile App built with React Native.",
+    techStack: ["React", "React-Native", "Expo"],
+    links: {
+      sourceCode: "",
+      websiteUrl: "",
+      youtubeDemo: "",
+    },
+    imageUrl: "",
   },
 ]
 
 module.exports = projects
+
+// my-reads
+// https://github.com/Ismail-Opatola/react-project-myreads-app
+// https://dazzling-ride-c69f41.netlify.com/
