6e9bee99341e81b93c7e54caa116ad228d410acd
Ismail Opatola <opatolamails@gmail.com>
2019-10-25 09:19:21 +0100

    update component -- sidebar, delete top-SwipableDrawer, apply zero-arg-functions, import copywright

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,85 +1,79 @@
 import React from "react"
+import clsx from "clsx"
+
 import { makeStyles } from "@material-ui/core/styles"
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer"
-import Grid from "@material-ui/core/Grid"
-import Box from "@material-ui/core/Box"
-// import Button from "@material-ui/core/Button"
-// import List from "@material-ui/core/List"
-// import Divider from "@material-ui/core/Divider"
-// import ListItem from "@material-ui/core/ListItem"
-// import ListItemIcon from "@material-ui/core/ListItemIcon"
-// import ListItemText from "@material-ui/core/ListItemText"
-// import InboxIcon from "@material-ui/icons/MoveToInbox"
-// import MailIcon from "@material-ui/icons/Mail"
+// import Grid from "@material-ui/core/Grid"
+// import Box from "@material-ui/core/Box"
+// import im from "../images/sidebar_bg@2x.png"
 import Nav from "./Nav"
+import Copyright from "./Copyright"
 
-const useStyles = makeStyles({
-  list: {
+const useStyles = makeStyles(theme => ({
+  nav: {
     width: "90vw",
-    margin: 'auto',
-    // justifyContent: "center",
-    // alignItems: "center",
-  },
-  fullList: {
-    width: "auto",
-  },
-})
+    height: "100%",
+    display: "flex",
 
-export default function SwipeableTemporaryDrawer({
-  topDrawer,
-  leftDrawer,
-  toggleDrawer,
-}) {
+    // flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    flexGrow: 1
+
+    // backgroundImage: `url(${im})`,
+    // backgroundPosition: "center",
+    // backgroundBlend: "blur",
+    // backgroundSize: "fit",
+
+    // opacity: 0.5,
+
+    // height: "85vh",
+    // "background-image": `linear-gradient(
+    // to right bottom,
+    // rgba($color-primary-light, 0.8),
+    // rgba($color-primary-dark, 0.8)),
+    // url(${im})`, // DPR: 1.0 load the small img for screen with lower resolution
+    // "background-size": "cover",
+    // "background-position": "top",
+  },
+  // fullList: {
+  //   width: "auto",
+  // },
+}))
+
+export default function SwipeableTemporaryDrawer({ leftDrawer, toggleDrawer }) {
   const classes = useStyles()
+  const mixedClasses = clsx(classes.nav, "purple-bg")
 
-  const sideList = side => (
+  const openDrawer = () => toggleDrawer("left", true);
+  const closeDrawer = () => toggleDrawer("left", false);
+
+  const sideList = () => (
     <nav
-      className={classes.list}
+      className={mixedClasses}
       role="presentation"
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    //   justify="center"
-    //   alignContent="center"
-    //   container
+      onClick={closeDrawer()}
+      onKeyDown={closeDrawer()}
     >
       <Nav />
+      {/*<div>
+        <Copyright />
+      </div>*/}
     </nav>
   )
 
-  const fullList = side => (
-    <nav
-      className={classes.fullList}
-      role="presentation"
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    >
-      <Nav />
-    </nav>
-  )
   const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent)
 
   return (
     <>
-      {/* <Button onClick={toggleDrawer("left", true)}>Open Left</Button> */}
-      {/* <Button onClick={toggleDrawer("top", true)}>Open Top</Button> */}
       <SwipeableDrawer
         open={leftDrawer}
-        onClose={toggleDrawer("left", false)}
-        onOpen={toggleDrawer("left", true)}
+        onClose={closeDrawer()}
+        onOpen={openDrawer()}
         disableBackdropTransition={!iOS}
         disableDiscovery={iOS}
       >
-        {sideList("left")}
-      </SwipeableDrawer>
-      <SwipeableDrawer
-        anchor="top"
-        open={topDrawer}
-        onClose={toggleDrawer("top", false)}
-        onOpen={toggleDrawer("top", true)}
-        disableBackdropTransition={!iOS}
-        disableDiscovery={iOS}
-      >
-        {fullList("top")}
+        {sideList()}
       </SwipeableDrawer>
     </>
   )
