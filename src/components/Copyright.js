8f48300050abd033c1042bb12200b5b56dc4e299
Ismail Opatola <opatolamails@gmail.com>
2019-10-25 09:30:16 +0100

    update component -- copyright

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,10 +1,14 @@
-function Copyright() {
+import React from "react"
+import Typography from "@material-ui/core/Typography"
+import Link from "./Link"
+
+export default function Copyright() {
     return (
-      <Typography variant="body2" color="textSecondary" align="center">
+      <Typography variant="body2" color="textSecondary" align="center" display="block">
         {'Copyright © '}
-        <MuiLink color="inherit" href="https://material-ui.com/">
-          Your Website
-        </MuiLink>{' '}
+        <Link color="inherit" to={"/"}>
+          Ismail Development
+        </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
       </Typography>
