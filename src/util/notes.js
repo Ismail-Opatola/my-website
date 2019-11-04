1d64f3e5c41a9a29eabda2f6d304081f589016de
Ismail Opatola <opatolamails@gmail.com>
2019-11-04 12:08:10 +0100

    create util -- notes

---
+++ src/util/notes.js
@@ -0,0 +1,130 @@
+const projects = [
+  {
+    title: 'Booho clone',
+    description:
+      'A clone of Boohoo Fashion UK. A client facing ecommerce web application built on Nodejs, Nextjs and Google Firebase Services.',
+    techStack: [
+      'Firebase Auth',
+      'Nextjs',
+      'Nodejs',
+      'Firebase Hosting',
+      'Firebase Cloud Functions',
+      'Firebase Firestore',
+      'Typescript',
+    ],
+    features: ['xxgx', 'xxexx', 'xxtxx', 'xxgjx', 'xxppx', 'xxffx', 'xdsx'],
+    links: {
+      sourceCode: 'xxxx',
+      websiteUrl: '',
+      youtubeDemo: 'xxxx',
+    },
+    imageUrl: '',
+  },
+  {
+    title: 'Boohoo CMS',
+    description:
+      'A fully featured Content Management System for Bohoo-Clone. An ecommerce platform built on Node, Nextjs and Google Firebase Services.',
+    techStack: [
+      'Firebase Auth',
+      'Nextjs',
+      'Nodejs',
+      'Firebase Hosting',
+      'Firebase Cloud Functions',
+      'Firebase Firestore',
+      'Typescript',
+    ],
+    features: ['xuixx', 'xoixx', 'sexxx', 'xxkhx', 'xgddxx', 'xxggdx', 'xxrexx'],
+    links: {
+      sourceCode: 'xxxx',
+      websiteUrl: 'xxxx',
+      youtubeDemo: 'xxxx',
+    },
+    imageUrl: '',
+  },
+  {
+    title: 'WUR',
+    description:
+      "Would-You-Rather App. A project Inspired by Udacity's React Developer Nano Degree programe. This app was part of the course capstone project..",
+    techStack: [
+      'Firebase Auth',
+      'React',
+      'Express',
+      'Firebase Hosting',
+      'Firebase Cloud Functions',
+      'Firebase Firestore',
+      'Material-UI',
+    ],
+    features: ['xxx', 'xxxx', 'xxxxx', 'oxxxx', 'xoxxx', 'xxoxx', 'xxxox'],
+    links: {
+      sourceCode: 'https://github.com/Ismail-Opatola/wur-react-firebase-client',
+      websiteUrl: 'https://would-you-rather-app-c5895.firebaseapp.com',
+      youtubeDemo: '',
+    },
+    imageUrl: 'wur_img.png',
+  },
+  {
+    title: 'Estatement',
+    description:
+      'A real estate management and architectural business website built with Gatsby and Netlify service.',
+    techStack: ['Gatsby', 'React', 'Markdown', 'Reactstrap', 'Bootstrap', 'Netlify'],
+    features: ['xxxxo', 'ooxxx', 'oooxx', 'oooox', 'xyxxx', 'xxixx', 'xxgxx'],
+    links: {
+      sourceCode: 'https://github.com/Ismail-Opatola/estatement_website',
+      websiteUrl: 'https://inspiring-gates-7b3e39.netlify.com/',
+      youtubeDemo: 'xxxx',
+    },
+    imageUrl: 'estatement_img.png',
+  },
+
+  {
+    title: 'Fit Tracker',
+    description: 'A Fitness Tracking Mobile App built with React Native.',
+    techStack: ['React', 'React-Native', 'Expo', 'Redux'],
+    features: ['xxyrex', 'xaffx', 'xljgx', 'bnxx', 'xxorex', 'xxldjx', 'xxtnxx'],
+    links: {
+      sourceCode: 'https://github.com/Ismail-Opatola/fit_tracker_app-',
+      websiteUrl: 'xxxx',
+      youtubeDemo: 'xxxx',
+    },
+    imageUrl: '',
+  },
+];
+
+const skills = {
+  programming: ['Javascript 100%', 'Typescripts 60%', 'HTML 100%', 'CSS 100%'],
+  techStack: [
+    'React 100%',
+    'React Native 80%',
+    'Nodejs 90%',
+    'MongoDB 80%',
+    'Express 90%',
+    'Firebase 90%',
+    'Gatsby 80%',
+    'Nextjs 80%',
+    'GraphQL 70%',
+    'SCSS 90%',
+    'Material-UI 90%',
+    'GSAP',
+    'Shopify',
+  ],
+  tools: [
+    'Git 100%',
+    'Vscode 100%',
+    'Adobe XD 100%',
+    'Inkscape 50%',
+    'Vim 40%',
+    'Google Web Console',
+    'SEMrush',
+    'Moosend',
+    'Mailchimp',
+    'Postman',
+    'Genymotion',
+    'VirtualBox',
+  ],
+};
+// const Notes = ;
+module.exports = notes = { projects, skills };
+
+// my-reads
+// https://github.com/Ismail-Opatola/react-project-myreads-app
+// https://dazzling-ride-c69f41.netlify.com/
