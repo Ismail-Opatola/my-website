b872a5c31b213cd9f202a6f1e3b8b20b1fa722fb
Ismail Opatola <opatolamails@gmail.com>
2019-11-07 12:39:48 +0100

    update component -- Layout, manage PopupForm state, pass props to Header, Footer

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -5,13 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { Fragment } from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 import {
   MuiThemeProvider,
   // createMuiTheme
-} from "@material-ui/core/styles"
+} from '@material-ui/core/styles';
 // import React from 'react';
 // import PropTypes from 'prop-types';
 // import AppBar from '@material-ui/core/AppBar';
@@ -27,16 +27,16 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 // import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 // import Zoom from '@material-ui/core/Zoom';
 
-import theme from "../../util/theme"
+import theme from '../../util/theme';
 
-import Header from "./Header"
-import SideBar from "./Sidebar"
-import Footer from "./Footer";
+import Header from './Header';
+import SideBar from './Sidebar';
+import Footer from './Footer';
 
 // import Container from "@material-ui/core/Container"
 // import Toolbar from "@material-ui/core/Toolbar"
 
-import "../../styles/index.scss"
+import '../../styles/index.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -47,42 +47,50 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
+  // Sidebar Drawer
   const [state, setState] = React.useState({
     left: false,
-  })
-
-  const toggleDrawer = (side, open) => event => {
-    if (
-      event &&
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return
+  });
+  const toggleDrawer = (side, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
     }
+    setState({ ...state, [side]: open });
+  };
 
-    setState({ ...state, [side]: open })
-  }
+  // FORM POPUP DIALOG
+  const [openFormDialog, setOpenFormDialog] = React.useState(false);
+  const handleOpenFormDialog = () => {
+    setOpenFormDialog(true);
+  };
+  const handleCloseFormDialog = () => {
+    setOpenFormDialog(false);
+  };
 
   return (
     <MuiThemeProvider theme={theme}>
-      <Fragment>
-      <CssBaseline />
+      <>
+        <CssBaseline />
         <Header
           toggleDrawer={toggleDrawer}
+          handleClickOpen={handleOpenFormDialog}
           // prop drilling -- useReducer
         />
-        <SideBar leftDrawer={state.left} toggleDrawer={toggleDrawer} />
+        <SideBar
+          leftDrawer={state.left} toggleDrawer={toggleDrawer} />
         <main>{children}</main>
-        <Footer/>
-      </Fragment>
+        <Footer
+          openFormDialog={openFormDialog} handleCloseFormDialog={handleCloseFormDialog}
+        />
+      </>
     </MuiThemeProvider>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
