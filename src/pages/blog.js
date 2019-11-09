2c18948bf1c0a3b2ab542ebbaca503947e680532
Ismail Opatola <opatolamails@gmail.com>
2019-11-09 22:52:34 +0100

    create page -- blog

---
+++ src/pages/blog.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from '../components/layout/Layout'
+import seo from '../components/seo'
+
+
+
+const BlogPage = () => {
+
+  return (
+    <Layout>
+    <seo title="blog" />
+      <Box>
+        Blog page...
+      </Box>
+    </Layout>
+  )
+
+}
+
+export default BlogPage
