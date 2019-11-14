608ec796f44aa356018ef77c72fbf3a444a6ed83
Ismail Opatola <opatolamails@gmail.com>
2019-11-14 22:22:59 +0100

    update notes

--- src/util/notes.js
+++ src/util/notes.js
@@ -128,3 +128,362 @@ module.exports = notes = { projects, skills };
 // my-reads
 // https://github.com/Ismail-Opatola/react-project-myreads-app
 // https://dazzling-ride-c69f41.netlify.com/
+/*
+
+
+{
+  "data": {
+    "post": {
+      "body": {
+        "json": {
+          "data": {},
+          "content": [
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": "The first thing you should do whenever you’re about to learn something new is ask yourself two questions -\r",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "paragraph"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "content": [
+                    {
+                      "data": {},
+                      "content": [
+                        {
+                          "data": {},
+                          "marks": [],
+                          "value": "Why does this thing exist?",
+                          "nodeType": "text"
+                        }
+                      ],
+                      "nodeType": "paragraph"
+                    }
+                  ],
+                  "nodeType": "list-item"
+                },
+                {
+                  "data": {},
+                  "content": [
+                    {
+                      "data": {},
+                      "content": [
+                        {
+                          "data": {},
+                          "marks": [],
+                          "value": "What problems does this thing solve?",
+                          "nodeType": "text"
+                        }
+                      ],
+                      "nodeType": "paragraph"
+                    }
+                  ],
+                  "nodeType": "list-item"
+                }
+              ],
+              "nodeType": "ordered-list"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": "If you never develop a convincing answer for both of those questions, you won’t have a solid enough foundation to build upon when you dive into the specifics. These questions are specifically interesting in regards to ",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {
+                    "uri": "https://tylermcginnis.com/courses/react-hooks/"
+                  },
+                  "content": [
+                    {
+                      "data": {},
+                      "marks": [],
+                      "value": "React Hooks",
+                      "nodeType": "text"
+                    }
+                  ],
+                  "nodeType": "hyperlink"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": ". React was the ",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {
+                    "uri": "https://insights.stackoverflow.com/survey/2019"
+                  },
+                  "content": [
+                    {
+                      "data": {},
+                      "marks": [],
+                      "value": "most popular and most loved front-end framework",
+                      "nodeType": "text"
+                    }
+                  ],
+                  "nodeType": "hyperlink"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": " in the JavaScript ecosystem when Hooks were released. Despite the existing praise, the React team still saw it necessary to build and release Hooks. Lost in the various Medium posts and blog think pieces on Hooks are the reasons (1) why and for what (2) benefit, despite high praise and popularity, the React team decided to spend valuable resources building and releasing Hooks. To better understand the answers to both of these questions, we first need to take a deeper look into how we’ve historically written React apps.\r\n",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "paragraph"
+            },
+            {
+              "data": {
+                "target": {
+                  "sys": {
+                    "space": {
+                      "sys": {
+                        "type": "Link",
+                        "linkType": "Space",
+                        "id": "quhv6kh057t5"
+                      }
+                    },
+                    "id": "c3evH4fRWwISRdgQLpq8c26",
+                    "type": "Asset",
+                    "createdAt": "2019-11-10T03:43:29.838Z",
+                    "updatedAt": "2019-11-10T03:43:29.838Z",
+                    "environment": {
+                      "sys": {
+                        "id": "master",
+                        "type": "Link",
+                        "linkType": "Environment"
+                      }
+                    },
+                    "revision": 1
+                  },
+                  "fields": {
+                    "title": {
+                      "en-US": "watch-internals"
+                    },
+                    "file": {
+                      "en-US": {
+                        "url": "//images.ctfassets.net/quhv6kh057t5/3evH4fRWwISRdgQLpq8c26/8f402098b52e197824b2e18759ebfb2e/watch-internals.jpg",
+                        "details": {
+                          "size": 358788,
+                          "image": {
+                            "width": 900,
+                            "height": 1210
+                          }
+                        },
+                        "fileName": "watch-internals.jpg",
+                        "contentType": "image/jpeg"
+                      }
+                    }
+                  }
+                }
+              },
+              "content": [],
+              "nodeType": "embedded-asset-block"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": "",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "paragraph"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": "createClass",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "heading-3"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": "If you’ve been around the React game long enough, you’ll remember the ",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [
+                    {
+                      "type": "code"
+                    }
+                  ],
+                  "value": "React.createClass",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": " API. It was the original way in which we’d create React components. All of the information you’d use to describe the component would be passed as an object to ",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [
+                    {
+                      "type": "code"
+                    }
+                  ],
+                  "value": "createClass",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": ".",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "paragraph"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [
+                    {
+                      "type": "code"
+                    }
+                  ],
+                  "value": "const ReposGrid = React.createClass({\r\n  getInitialState () {\r\n    return {\r\n      repos: [],\r\n      loading: true\r\n    }\r\n  },\r\n  componentDidMount () {\r\n    this.updateRepos(this.props.id)\r\n  },\r\n  componentDidUpdate (prevProps) {\r\n    if (prevProps.id !== this.props.id) {\r\n      this.updateRepos(this.props.id)\r\n    }\r\n  },\r\n  updateRepos (id) {\r\n    this.setState({ loading: true })\r\n\r\n    fetchRepos(id)\r\n      .then((repos) => this.setState({\r\n        repos,\r\n        loading: false\r\n      }))\r\n  },\r\n  render() {\r\n    const { loading, repos } = this.state\r\n\r\n    if (loading === true) {\r\n      return <Loading />\r\n    }\r\n\r\n    return (\r\n      <ul>\r\n        {repos.map(({ name, handle, stars, url }) => (\r\n          <li key={name}>\r\n            <ul>\r\n              <li><a href={url}>{name}</a></li>\r\n              <li>@{handle}</li>\r\n              <li>{stars} stars</li>\r\n            </ul>\r\n          </li>\r\n        ))}\r\n      </ul>\r\n    )\r\n  }\r\n})",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "paragraph"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": "\r\n",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {
+                    "uri": "https://codesandbox.io/s/why-react-hooks-createclass-iw6s5"
+                  },
+                  "content": [
+                    {
+                      "data": {},
+                      "marks": [],
+                      "value": "💻 Play with the code.",
+                      "nodeType": "text"
+                    }
+                  ],
+                  "nodeType": "hyperlink"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": "",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "paragraph"
+            },
+            {
+              "data": {},
+              "content": [
+                {
+                  "data": {},
+                  "marks": [
+                    {
+                      "type": "code"
+                    }
+                  ],
+                  "value": "createClass",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": " was a simple and effective way to create React components. The reason React initially used the ",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [
+                    {
+                      "type": "code"
+                    }
+                  ],
+                  "value": "createClass",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": " API was because, at the time, JavaScript didn’t have a built-in class system. Of course, this eventually changed. With ES6, JavaScript introduced the ",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [
+                    {
+                      "type": "code"
+                    }
+                  ],
+                  "value": "class",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": " keyword and with it a native way to create classes in JavaScript. This put React in a tough position. Either continue using ",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [
+                    {
+                      "type": "code"
+                    }
+                  ],
+                  "value": "createClass",
+                  "nodeType": "text"
+                },
+                {
+                  "data": {},
+                  "marks": [],
+                  "value": " and fight against the progression of JavaScript or submit to the will of the EcmaScript standard and embrace classes. As history has shown, they chose the later.",
+                  "nodeType": "text"
+                }
+              ],
+              "nodeType": "paragraph"
+            }
+          ],
+          "nodeType": "document"
+        }
+      }
+    }
+  }
+}
+
+*/
