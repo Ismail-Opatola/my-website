ada4aa8a66353a8a3abbf4fb5febf6ab6737d682
Ismail Opatola <opatolamails@gmail.com>
2019-11-15 07:09:35 +0100

    update gatsby-config -- configure sitemap, robot.txt plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,35 @@
-let activeEnv =
-  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
 
-console.log('Using environment config: ${activeEnv}')
+console.log('Using environment config: ${activeEnv}');
 
-require("dotenv").config({
+require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
+
+// Robot.txt -- disable crawlers for deploy-previews
+// https://www.gatsbyjs.org/packages/gatsby-plugin-robots-txt/
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = process.env.SITE_URL,
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
 module.exports = {
   siteMetadata: {
     title: 'Ismail Opatola',
     description: 'Web Developer | Full Stack React Developer',
     developer: 'Ismail Opatola',
+    siteUrl: 'https://www.ismailopatola.io',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-catch-links',
     'gatsby-transformer-remark',
+
     {
       resolve: 'gatsby-source-contentful',
       options: {
@@ -80,5 +92,71 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/some-other-sitemap.xml',
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        exclude: ['/category/*', '/path/to/page'],
+        query: `
+            {
+              site {
+                siteMetadata {
+                  siteUrl
+                }
+              }
+
+              allSitePage {
+                edges {
+                  node {
+                    path
+                  }
+                }
+              }
+          }`,
+        serialize: ({ site, allSitePage }) => allSitePage.edges.map((edge) => ({
+          url: site.siteMetadata.siteUrl + edge.node.path,
+          changefreq: 'daily',
+          priority: 0.7,
+        })),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        query: `{
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+        }`,
+        host: ({ site }) => `${site.siteMetadata.siteUrl}`,
+        sitemap: ({ site }) => `${data.site.siteMetadata.siteUrl}/sitemap.xml`,
+        // resolveEnv: () => process.env.GATSBY_ENV,
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          development: {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+          },
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
   ],
 };
