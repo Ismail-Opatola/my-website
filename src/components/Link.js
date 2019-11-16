14f42545f4d8abc123a20fd6b708607c21220920
Ismail Opatola <opatolamails@gmail.com>
2019-11-16 14:03:02 +0100

    update component -- Links, apply pink pallete to internal links

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import MuiLink from '@material-ui/core/Link';
 import { Link as GatsbyLink } from 'gatsby';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    color: theme.palette.text.secondary,
+    cursor: 'pointer'
+  }
+}))
 
 const Link = React.forwardRef(function Link(props, ref) {
-  return <MuiLink component={GatsbyLink} ref={ref} {...props} />;
+  const classes = useStyles()
+  return <MuiLink className={classes.root} component={GatsbyLink} ref={ref} {...props}  />;
 });
 
 export default Link
