ec6a09b86ee2444468bb0d5cd7a90ab80d69987b
Ismail Opatola <opatolamails@gmail.com>
2019-11-27 16:59:28 +0100

    update component -- video, add responsive iframe

--- src/components/video.js
+++ src/components/video.js
@@ -1,29 +1,59 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Button from "@material-ui/core/Button"
-import Dialog from "@material-ui/core/Dialog"
-import DialogActions from "@material-ui/core/DialogActions"
-import DialogContent from "@material-ui/core/DialogContent"
-// import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from "@material-ui/core/DialogTitle"
+import {
+ makeStyles,
+} from "@material-ui/core/styles"
 import Slide from "@material-ui/core/Slide"
+import Dialog from '@material-ui/core/Dialog'
+import Box from "@material-ui/core/Box"
 
+const useStyles = makeStyles(theme => ({
+ root: {
+  minHeight: "auto",
+  margin: "auto",
+  textAlign: "center",
+  overflow: "visible",
+  height: "fit-content",
+  [theme.breakpoints.down("xs")]: {  
+    margin: "auto"
+  },
+ },
+ videoWrapper: {
+	position: 'relative',
+	paddingBottom: '56.25%', /* 16:9 -- 16/9=0.5925*/
+	paddingTop: '25px',
+  height: 0,
+ },
+  iframe2: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  }
+}))
 const Transition = React.forwardRef((props, ref) => (
  <Slide direction="up" ref={ref} {...props} />
 ))
 
 export default function Video({ open, close, title, videoSrcUrl }) {
+ const classes = useStyles()
+
  return (
   <Dialog
    open={open}
+   maxWidth="md"
+   fullWidth
    TransitionComponent={Transition}
    keepMounted
    onClose={close}
    aria-labelledby={`{title}-project-demo`}
+   className={classes.root}
   >
-   <DialogTitle id={`{title}-project-demo`}>{title}</DialogTitle>
-   <DialogContent>
+   <Box 
+      className={classes.videoWrapper}
+   >
     <iframe
      src={videoSrcUrl}
      title={title}
@@ -32,22 +62,19 @@ export default function Video({ open, close, title, videoSrcUrl }) {
      webkitallowfullscreen="true"
      mozallowfullscreen="true"
      allowFullScreen
+     className={classes.iframe2}
+     width="560" height="349"
     />
-   </DialogContent>
-   <DialogActions>
-    <Button onClick={close} color="primary">
-     close
-    </Button>
-   </DialogActions>
+   </Box>
   </Dialog>
  )
 }
 
 Video.defaultProps = {
-    open: false,
-    title: 'demo',
-    videoSrcUrl: 'https://www.youtube.com/watch?v=ff4fgQxPaO0',
-};
+ open: false,
+ title: "demo",
+ videoSrcUrl: "https://www.youtube.com/embed/ff4fgQxPaO0",
+}
 
 Video.propTypes = {
  open: PropTypes.bool.isRequired,
