df465a643e691d0d34dcee66da3af6385a7713d0
Ismail Opatola <opatolamails@gmail.com>
2019-12-02 09:58:50 +0100

    update util -- notes, add ismailopatolaio to project list

--- src/util/notes.js
+++ src/util/notes.js
@@ -1,44 +1,78 @@
 const projects = [
+  // {
+  //  title: "Firebase cms",
+  // },
+  // {
+  //  title: "Booho clone",
+  //  description:
+  //   "A clone of Boohoo Fashion UK. A client facing ecommerce web application built on Nodejs, Nextjs and Google Firebase Services.",
+  //  techStack: [
+  //   "Firebase Auth",
+  //   "Nextjs",
+  //   "Nodejs",
+  //   "Firebase Hosting",
+  //   "Firebase Cloud Functions",
+  //   "Firebase Firestore",
+  //   "Typescript",
+  //  ],
+  //  features: ["xxgx", "xxexx", "xxtxx", "xxgjx", "xxppx", "xxffx", "xdsx"],
+  //  links: {
+  //   sourceCode: "xxxx",
+  //   websiteUrl: "",
+  //   youtubeDemo: "xxxx",
+  //  },
+  //  gifSrcUrl: "",
+  //  videoSrcUrl: "",
+  // },
+  // {
+  //  title: "Boohoo CMS",
+  //  description:
+  //   "A fully featured Content Management System for Bohoo-Clone. An ecommerce platform built on Node, Nextjs and Google Firebase Services.",
+  //  techStack: [
+  //   "Firebase Auth",
+  //   "Nextjs",
+  //   "Nodejs",
+  //   "Firebase Hosting",
+  //   "Firebase Cloud Functions",
+  //   "Firebase Firestore",
+  //   "Typescript",
+  //  ],
+  //  features: ["xuixx", "xoixx", "sexxx", "xxkhx", "xgddxx", "xxggdx", "xxrexx"],
+  //  links: {
+  //   sourceCode: "xxxx",
+  //   websiteUrl: "xxxx",
+  //   youtubeDemo: "xxxx",
+  //  },
+  //  gifSrcUrl: "",
+  //  videoSrcUrl: "",
+  // },
   {
-   title: "Booho clone",
-   description:
-    "A clone of Boohoo Fashion UK. A client facing ecommerce web application built on Nodejs, Nextjs and Google Firebase Services.",
+   title: "ISMAILOPATOLA.IO",
+   description: "My professional potfolio and blogging website",
    techStack: [
-    "Firebase Auth",
-    "Nextjs",
-    "Nodejs",
-    "Firebase Hosting",
-    "Firebase Cloud Functions",
-    "Firebase Firestore",
-    "Typescript",
+    "React",
+    "Gatsby",
+    "Netlify",
+    "Contentful",
+    "Material UI",
+    "Sass",
+    "React-Helmet",
+    "Google-analytics",
    ],
-   features: ["xxgx", "xxexx", "xxtxx", "xxgjx", "xxppx", "xxffx", "xdsx"],
+   features: [
+    "Blog",
+    "Responsive UI",
+    "SEO",
+    "Analytics",
+    "AXE Accessibility Test",
+    "Linting",
+    "Social Share",
+    "Social Comment with Discus"
+   ],
    links: {
-    sourceCode: "xxxx",
+    sourceCode: "https://github.com/Ismail-Opatola/my-website",
     websiteUrl: "",
-    youtubeDemo: "xxxx",
-   },
-   gifSrcUrl: "",
-   videoSrcUrl: "",
-  },
-  {
-   title: "Boohoo CMS",
-   description:
-    "A fully featured Content Management System for Bohoo-Clone. An ecommerce platform built on Node, Nextjs and Google Firebase Services.",
-   techStack: [
-    "Firebase Auth",
-    "Nextjs",
-    "Nodejs",
-    "Firebase Hosting",
-    "Firebase Cloud Functions",
-    "Firebase Firestore",
-    "Typescript",
-   ],
-   features: ["xuixx", "xoixx", "sexxx", "xxkhx", "xgddxx", "xxggdx", "xxrexx"],
-   links: {
-    sourceCode: "xxxx",
-    websiteUrl: "xxxx",
-    youtubeDemo: "xxxx",
+    youtubeDemo: "",
    },
    gifSrcUrl: "",
    videoSrcUrl: "",
@@ -50,25 +84,54 @@ const projects = [
    techStack: [
     "Firebase Auth",
     "React",
+    "Redux",
     "Express",
     "Firebase Hosting",
     "Firebase Cloud Functions",
     "Firebase Firestore",
-    "Material-UI",
+    "Material UI",
    ],
-   features: ["xxx", "xxxx", "xxxxx", "oxxxx", "xoxxx", "xxoxx", "xxxox"],
+   features: [
+    "Responsive UI",
+    "User Authentication",
+    "User Role & Authorization",
+    "User Profile",
+    "User Generated Survey",
+    "User Generated Polls",
+    "LeaderBorad",
+    "Real-time Notification",
+   ],
    links: {
     sourceCode: "https://github.com/Ismail-Opatola/wur-react-firebase-client",
     websiteUrl: "https://would-you-rather-app-c5895.firebaseapp.com",
     youtubeDemo: "",
    },
    gifSrcUrl: "",
-   videoSrcUrl: "wur_img.png",
+   videoSrcUrl: "",
+  },
+  {
+   title: "Fit Tracker",
+   description: "A Fitness Tracking Mobile App built with React Native.",
+   techStack: ["React", "React-Native", "Expo", "Redux"],
+   features: [
+    "Foot-Tracking",
+    "Daily Routin Check",
+    "Navigation Compass",
+    "Distance Calculation",
+    "Push Notification",
+   ],
+   links: {
+    sourceCode: "https://github.com/Ismail-Opatola/fit_tracker_app-",
+    websiteUrl: "",
+    youtubeDemo: "",
+   },
+   gifSrcUrl: "",
+   videoSrcUrl: "",
   },
   {
    title: "Estatement",
    description:
-    "A real estate management and architectural business website built with Gatsby and Netlify service.",
+    "A demo - real estate management and architectural business website built with Gatsby and Netlify service.",
    techStack: [
     "Gatsby",
     "React",
@@ -77,25 +140,11 @@ const projects = [
     "Bootstrap",
     "Netlify",
    ],
-   features: ["xxxxo", "ooxxx", "oooxx", "oooox", "xyxxx", "xxixx", "xxgxx"],
+   features: ["Responsive UI", "SEO", "LazyLoad Image"],
    links: {
     sourceCode: "https://github.com/Ismail-Opatola/estatement_website",
     websiteUrl: "https://inspiring-gates-7b3e39.netlify.com/",
-    youtubeDemo: "xxxx",
-   },
-   gifSrcUrl: "",
-   videoSrcUrl: "estatement_img.png",
-  },
-
-  {
-   title: "Fit Tracker",
-   description: "A Fitness Tracking Mobile App built with React Native.",
-   techStack: ["React", "React-Native", "Expo", "Redux"],
-   features: ["xxyrex", "xaffx", "xljgx", "bnxx", "xxorex", "xxldjx", "xxtnxx"],
-   links: {
-    sourceCode: "https://github.com/Ismail-Opatola/fit_tracker_app-",
-    websiteUrl: "xxxx",
-    youtubeDemo: "xxxx",
+    youtubeDemo: "",
    },
    gifSrcUrl: "",
    videoSrcUrl: "",
@@ -110,394 +159,32 @@ const projects = [
    "MongoDB 80%",
    "Express 90%",
    "Firebase 90%",
-   "Gatsby 80%",
+   "Gatsby 90%",
    "Nextjs 80%",
    "GraphQL 70%",
    "SCSS 90%",
    "Material-UI 90%",
-   "GSAP",
+   "Contentful",
    "Shopify",
+   "GSAP",
   ],
   tools: [
    "Git 100%",
-   "Vscode 100%",
+   "Vscode",
    "Adobe XD 100%",
-   "Inkscape 50%",
+   "Inkscape 70%",
    "Vim 40%",
-   "Google Web Console",
-   "SEMrush",
-   "Moosend",
-   "Mailchimp",
-   "Postman",
    "Genymotion",
    "VirtualBox",
+   "Axe-Accessibilty",
+   "Postman",
+   "Google Web Console",
+   "Google Analytics 50%",
+   "SEMrush",
+   "Mailchimp",
+   "Moosend",
   ],
  }
 
 const notes = { projects, skills }
 module.exports = notes
-
-/*
- * My-reads
- * https://github.com/Ismail-Opatola/react-project-myreads-app
- * https://dazzling-ride-c69f41.netlify.com/
- */
-/*
- *
- *
- * {
- * "data": {
- * "post": {
- * "body": {
- * "json": {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "The first thing you should do whenever you’re about to learn something new is ask yourself two questions -\r",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "Why does this thing exist?",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * }
- * ],
- * "nodeType": "list-item"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "What problems does this thing solve?",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * }
- * ],
- * "nodeType": "list-item"
- * }
- * ],
- * "nodeType": "ordered-list"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "If you never develop a convincing answer for both of those questions, you won’t have a solid enough foundation to build upon when you dive into the specifics. These questions are specifically interesting in regards to ",
- * "nodeType": "text"
- * },
- * {
- * "data": {
- * "uri": "https://tylermcginnis.com/courses/react-hooks/"
- * },
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "React Hooks",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "hyperlink"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": ". React was the ",
- * "nodeType": "text"
- * },
- * {
- * "data": {
- * "uri": "https://insights.stackoverflow.com/survey/2019"
- * },
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "most popular and most loved front-end framework",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "hyperlink"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": " in the JavaScript ecosystem when Hooks were released. Despite the existing praise, the React team still saw it necessary to build and release Hooks. Lost in the various Medium posts and blog think pieces on Hooks are the reasons (1) why and for what (2) benefit, despite high praise and popularity, the React team decided to spend valuable resources building and releasing Hooks. To better understand the answers to both of these questions, we first need to take a deeper look into how we’ve historically written React apps.\r\n",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * },
- * {
- * "data": {
- * "target": {
- * "sys": {
- * "space": {
- * "sys": {
- * "type": "Link",
- * "linkType": "Space",
- * "id": "quhv6kh057t5"
- * }
- * },
- * "id": "c3evH4fRWwISRdgQLpq8c26",
- * "type": "Asset",
- * "createdAt": "2019-11-10T03:43:29.838Z",
- * "updatedAt": "2019-11-10T03:43:29.838Z",
- * "environment": {
- * "sys": {
- * "id": "master",
- * "type": "Link",
- * "linkType": "Environment"
- * }
- * },
- * "revision": 1
- * },
- * "fields": {
- * "title": {
- * "en-US": "watch-internals"
- * },
- * "file": {
- * "en-US": {
- * "url": "//images.ctfassets.net/quhv6kh057t5/3evH4fRWwISRdgQLpq8c26/8f402098b52e197824b2e18759ebfb2e/watch-internals.jpg",
- * "details": {
- * "size": 358788,
- * "image": {
- * "width": 900,
- * "height": 1210
- * }
- * },
- * "fileName": "watch-internals.jpg",
- * "contentType": "image/jpeg"
- * }
- * }
- * }
- * }
- * },
- * "content": [],
- * "nodeType": "embedded-asset-block"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "createClass",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "heading-3"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "If you’ve been around the React game long enough, you’ll remember the ",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [
- * {
- * "type": "code"
- * }
- * ],
- * "value": "React.createClass",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": " API. It was the original way in which we’d create React components. All of the information you’d use to describe the component would be passed as an object to ",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [
- * {
- * "type": "code"
- * }
- * ],
- * "value": "createClass",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": ".",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [
- * {
- * "type": "code"
- * }
- * ],
- * "value": "const ReposGrid = React.createClass({\r\n  getInitialState () {\r\n    return {\r\n      repos: [],\r\n      loading: true\r\n    }\r\n  },\r\n  componentDidMount () {\r\n    this.updateRepos(this.props.id)\r\n  },\r\n  componentDidUpdate (prevProps) {\r\n    if (prevProps.id !== this.props.id) {\r\n      this.updateRepos(this.props.id)\r\n    }\r\n  },\r\n  updateRepos (id) {\r\n    this.setState({ loading: true })\r\n\r\n    fetchRepos(id)\r\n      .then((repos) => this.setState({\r\n        repos,\r\n        loading: false\r\n      }))\r\n  },\r\n  render() {\r\n    const { loading, repos } = this.state\r\n\r\n    if (loading === true) {\r\n      return <Loading />\r\n    }\r\n\r\n    return (\r\n      <ul>\r\n        {repos.map(({ name, handle, stars, url }) => (\r\n          <li key={name}>\r\n            <ul>\r\n              <li><a href={url}>{name}</a></li>\r\n              <li>@{handle}</li>\r\n              <li>{stars} stars</li>\r\n            </ul>\r\n          </li>\r\n        ))}\r\n      </ul>\r\n    )\r\n  }\r\n})",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "\r\n",
- * "nodeType": "text"
- * },
- * {
- * "data": {
- * "uri": "https://codesandbox.io/s/why-react-hooks-createclass-iw6s5"
- * },
- * "content": [
- * {
- * "data": {},
- * "marks": [],
- * "value": "💻 Play with the code.",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "hyperlink"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": "",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * },
- * {
- * "data": {},
- * "content": [
- * {
- * "data": {},
- * "marks": [
- * {
- * "type": "code"
- * }
- * ],
- * "value": "createClass",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": " was a simple and effective way to create React components. The reason React initially used the ",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [
- * {
- * "type": "code"
- * }
- * ],
- * "value": "createClass",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": " API was because, at the time, JavaScript didn’t have a built-in class system. Of course, this eventually changed. With ES6, JavaScript introduced the ",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [
- * {
- * "type": "code"
- * }
- * ],
- * "value": "class",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": " keyword and with it a native way to create classes in JavaScript. This put React in a tough position. Either continue using ",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [
- * {
- * "type": "code"
- * }
- * ],
- * "value": "createClass",
- * "nodeType": "text"
- * },
- * {
- * "data": {},
- * "marks": [],
- * "value": " and fight against the progression of JavaScript or submit to the will of the EcmaScript standard and embrace classes. As history has shown, they chose the later.",
- * "nodeType": "text"
- * }
- * ],
- * "nodeType": "paragraph"
- * }
- * ],
- * "nodeType": "document"
- * }
- * }
- * }
- * }
- * }
- *
- */
